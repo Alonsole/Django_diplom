e62abbfd0c9bb5ea334ae015897d8e9a1b5b5a5e
Alonsole <157732573+Alonsole@users.noreply.github.com>
2025-02-18 22:44:38 +0300

    Update Django settings: added Celery broker and result backend configs
    Добавил настройки Celery для подключения к Redis

--- app/netology_pd_diplom/settings.py
+++ app/netology_pd_diplom/settings.py
@@ -163,3 +163,7 @@ REST_FRAMEWORK = {
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Настройки Celery для Djando
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/1')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379/2')
